bc71fcccffde63dd3a074432be91292f1234dfd2
Nate Finch <finchproservices@gmail.com>
2016-03-17 10:10:07 +0900

    Materials Pages added

--- materials-4-col.php
+++ materials-4-col.php
@@ -6,6 +6,48 @@
 
 
 
+add_action( 'genesis_loop', 'get_the_materials_columns_4');
+
+function get_the_materials_columns_4() {
+
+	echo '<div class="materials-page-repeater-outer">';
+	// check if the repeater field has rows of data
+	if( have_rows('material_information') ):
+
+		// loop through the rows of data
+		while ( have_rows('material_information') ) : the_row();
+
+			$title = get_sub_field('material_title');
+			$image = get_sub_field('material_image');
+			$content = get_sub_field('material_paragraph');
+
+			echo '<div class="materials-page-repeater-inner-4-col">';
+
+			echo '<img src="' . $image . '" >';
+
+			echo '<h2>' . $title . '</h2>';
+
+			echo $content;
+
+			echo '</div>';// end materials-page-repeater-inner
+
+		endwhile;
+
+	else :
+
+		// no rows found
+
+	endif;
+
+	echo '</div>';// end materials-page-repeater-outer
+
+}
+
+
+
+
+
+
 
 
 
